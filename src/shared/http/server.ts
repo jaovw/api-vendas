920d32c96b8060c90400ff09991799b69ec7817c
jaovw <joojvw@gmail.com>
2022-12-21 13:37:58 -0300

    server config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,2 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+const port: number = 3000 || process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`API rodando na porta ${port}`);
+});
