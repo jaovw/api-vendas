2aa6d58e62721f601a287771e1ab7f3a0e533d31
jaovw <joojvw@gmail.com>
2022-12-24 00:17:50 -0300

    servico com metodo para atualizar produto

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import ProductRepository from '../typeorm/repositories/ProductsRepository';
+import IRequest from './interfaces/IRequest';
+
+export default class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Produto nao encontrado.');
+    }
+
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('Ja existe um produto com esse nome.');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
