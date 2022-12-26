2f42dbeacc0460a607b6542f2f91826180e878d3
jaovw <joojvw@gmail.com>
2022-12-26 00:59:06 -0300

    product router

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,8 @@
+import productRouter from '@modules/products/routes/products.routes';
 import { Request, Response, Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req: Request, res: Response) => {
-  return res.json({
-    message: 'Hello World',
-  });
-});
+routes.use('/products', productRouter);
 
 export default routes;
