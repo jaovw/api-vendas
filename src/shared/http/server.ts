55e01a1752d1a647a9afc12b2966e7a9b72cb6c1
jaovw <joojvw@gmail.com>
2022-12-26 01:16:07 -0300

    tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import { errors } from 'celebrate';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -12,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 //  MIDDLEWARE PARA ERROS
 app.use(
