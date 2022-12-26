369baedd21f489535217db0e25e963e29548fb7a
jaovw <joojvw@gmail.com>
2022-12-26 01:25:18 -0300

    calidacao de dados request

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,57 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductsController';
-
+import { celebrate, Joi, Segments } from 'celebrate';
 const productRouter = Router();
 const poroductsController = new ProductController();
 
 productRouter.get('/', poroductsController.index);
-productRouter.get('/:id', poroductsController.show);
-productRouter.post('/', poroductsController.create);
-productRouter.put('/:id', poroductsController.update);
-productRouter.delete('/:id', poroductsController.delete);
+
+//  APLICANDO VALIDACAO DE REQUISICAO BODY/PARAM UTILIZANDO O CELEBRATE
+productRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  poroductsController.show,
+);
+
+productRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  poroductsController.create,
+);
+
+productRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  poroductsController.update,
+);
+
+productRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  poroductsController.delete,
+);
 
 export default productRouter;
