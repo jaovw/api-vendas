98ff13aeef70274ea747ec39a78761954a053fed
jaovw <joojvw@gmail.com>
2022-12-28 19:34:06 -0300

    rota de auth

---
+++ src/modules/users/routes/sessions.routes.ts
@@ -0,0 +1,20 @@
+import { celebrate, Segments } from 'celebrate';
+import { Router } from 'express';
+import Joi from 'joi';
+import SessionController from '../controllers/SessionsController';
+
+const sessionsRouter = Router();
+const sessionsController = new SessionController();
+
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsController.create
+);
+
+export default sessionsRouter
