6cdd2d80648ea268fbb741fa1f00eafd04bc57b6
jaovw <joojvw@gmail.com>
2022-12-28 17:59:07 -0300

    criptografando

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -19,10 +20,12 @@ export default class CreateUserService {
       throw new AppError('Email ja em uso.');
     }
 
+    const hashadPassword = await hash(password, 8)
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashadPassword,
     });
 
     await usersRepository.save(user);
