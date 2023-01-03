66c4a40316e0c52d93f8a706ad7d5b686e8baeb5
jaovw <joojvw@gmail.com>
2023-01-03 15:25:00 -0300

    email config

---
+++ src/config/mail/EtherealMail.ts
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+
+interface ISendMail {
+  to: string;
+  body: string;
+}
+export default class EtherealMail {
+  static async sendMail({ to, body }: ISendMail): Promise<void> {
+    const account = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+
+    const message = await transporter.sendMail({
+      from: 'equipe@apivendas.com.br',
+      to,
+      subject: 'Recuperacao de senha',
+      text: body,
+    });
+
+    console.log('Mensagem enviada: %s', message.messageId);
+    console.log('Previa da URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
