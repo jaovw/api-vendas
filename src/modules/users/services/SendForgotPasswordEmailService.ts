2f76c3da1a003da34b58d6c4058ea72820c1a4e5
jaovw <joojvw@gmail.com>
2023-01-02 23:36:32 -0300

    servico com metodo para geracao de token

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,25 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+export default class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Usuario nao existe.');
+    }
+
+    const token = await userTokenRepository.generate(user.id);
+
+    console.log(token);
+  }
+}
