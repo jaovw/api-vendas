d70feb6877907718c37c282d2398122e8e61fd3b
jaovw <joojvw@gmail.com>
2023-01-07 16:15:16 -0300

    entidade de order_product com relacionameto

---
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -0,0 +1,36 @@
+import Product from '@modules/products/typeorm/entities/Product';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import Order from './Order';
+
+@Entity('orders_products')
+export default class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+
+  @ManyToOne(() => Product, order => order.order_products)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
