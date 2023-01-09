c299bf1021f48c4586dc20f9cfd1e0a58475a4ad
jaovw <joojvw@gmail.com>
2023-01-08 22:58:12 -0300

    correcao de nome pasta e imports

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,9 +1,9 @@
 import { getCustomRepository } from 'typeorm';
-import Costumer from '../typeorm/entities/Customer';
+import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
 export default class ListCustomerService {
-  public async execute(): Promise<Costumer[]> {
+  public async execute(): Promise<Customer[]> {
     const customersRespository = getCustomRepository(CustomersRepository);
 
     const customers = customersRespository.find();
