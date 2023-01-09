511929feeeccfa628eb3c438604bace114381309
jaovw <joojvw@gmail.com>
2023-01-08 22:57:33 -0300

    novas rotas na raiz

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,11 @@
+import { Router } from 'express';
 import productRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
-import customersRouter from '@modules/costumers/routes/customers.routes';
-import { Router } from 'express';
+import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -14,4 +15,6 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
+
 export default routes;
